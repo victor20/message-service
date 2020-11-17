e2a4d8bd1001bb38f75cc80e072c835d60fcc309
pregen <pregen@kth.se>
2020-11-17 09:20:02 +0100

    ...

--- messageservice/__init__.py
+++ messageservice/__init__.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_ECHO'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 from messageservice.routes import blueprint
