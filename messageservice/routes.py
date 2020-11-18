379886a5dd7e8e1bd5cb9721dee8d1bbfb68b371
pregen <pregen@kth.se>
2020-11-18 05:12:40 +0100

    ...

--- messageservice/routes.py
+++ messageservice/routes.py
@@ -10,20 +10,12 @@ blueprint = Blueprint('api', __name__)
 @blueprint.route("/api/users", methods=['POST'])
 def add_user():
     validate_user(request.json)
-
     if User.query.filter_by(user_name=request.json['user_name']).first():
         abort(409, "Error username already in use")
-
     user = User()
     user.import_data(request.json)
     db.session.add(user)
-
-    try:
-        db.session.commit()
-    except Exception:
-        db.session.rollback()
-        raise
-
+    db.session.commit()
     return jsonify({"message": "User successfully created"}), 201
 
 
@@ -37,21 +29,13 @@ def get_users():
 def add_message(user_name: str):
     user = get_user_or_404(user_name)
     validate_message(request.json)
-
     receiver = User.query.filter_by(user_name=request.json['receiver']).first()
     if not receiver:
         raise ValidationError("Receiver not found")
-
     message = Message(sender=user, receiver=receiver)
     message.import_data(request.json)
     db.session.add(message)
-
-    try:
-        db.session.commit()
-    except Exception:
-        db.session.rollback()
-        raise
-
+    db.session.commit()
     return jsonify({"message": "Message successfully sent"}), 201
 
 
@@ -69,11 +53,9 @@ def get_messages_by_index(user_name):
     to_index = request.args.get('to')
     validate_index({'from_index': from_index, 'to_index': to_index})
     messages = [message.export_data() for message in user.received_messages[int(from_index)-1:int(to_index)-1]]
-
     if messages:
         user.add_latest_message_id(messages[-1]['id'])
         db.session.commit()
-
     return jsonify({'messages': messages})
 
 
@@ -82,11 +64,9 @@ def get_new_messages(user_name):
     user = get_user_or_404(user_name)
     messages = [message.export_data() for message in
                 Message.query.filter(Message.receiver==user, Message.id > user.latest_message_id).all()]
-
     if messages:
         user.add_latest_message_id(messages[-1]['id'])
         db.session.commit()
-
     return jsonify({'messages': messages})
 
 
